7b1b27cdeae8c68f593e72beb1581a0aae50d841
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-03-22 20:39:27 +0100

    adapting test to pass under -neo:o1

--- test/files/run/constant-type.scala
+++ test/files/run/constant-type.scala
@@ -1,4 +1,5 @@
 import scala.tools.partest.ReplTest
+import scala.tools.nsc.Settings
 
 // see the commit message to understand what this stuff is about
 // just a quick note:
@@ -6,6 +7,12 @@ import scala.tools.partest.ReplTest
 // as explained in the commit message, Type.erasure won't do for this test
 // because it does some postprocessing to the result of transformedType
 object Test extends ReplTest {
+
+  override def transformSettings(s: Settings): Settings = {
+    s.neo.value = "GenBCode"
+    s
+  }
+
   def code = """
 :power
 val s = transformedType(StringClass.toType).asInstanceOf[Type]
